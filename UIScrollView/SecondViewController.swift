2a51c1f852a6395dd72a79286ac98ff9ff52c63b
papadoster <papadoster13@gmail.com>
2023-01-04 23:30:41 +0100

    Initial Commit

---
+++ UIScrollView/SecondViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SecondViewController.swift
+//  UIScrollView
+//
+//  Created by Marina Karpova on 05.01.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var myScrollView = UIScrollView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let iphoneImage = UIImage(named: "iphone2")
+        let macImage = UIImage(named: "mac")
+        let ipadImage = UIImage(named: "ipad")
+        
+        let scrollViewRect = view.bounds
+        
+        myScrollView = UIScrollView(frame: scrollViewRect)
+        myScrollView.isPagingEnabled = true
+        myScrollView.contentSize = CGSize(width: scrollViewRect.width * 3, height: scrollViewRect.height)
+        view.addSubview(myScrollView)
+        
+        var imageViewRect = view.bounds
+        let iphoneImageView = self.newImageViewWithImage(image: iphoneImage!, frame: imageViewRect)
+        myScrollView.addSubview(iphoneImageView)
+        
+        imageViewRect.origin.x += iphoneImageView.frame.width
+        let ipadImageView = self.newImageViewWithImage(image: ipadImage!, frame: imageViewRect)
+        myScrollView.addSubview(ipadImageView)
+        
+        imageViewRect.origin.x += iphoneImageView.frame.width
+        let macImageView = self.newImageViewWithImage(image: macImage!, frame: imageViewRect)
+        myScrollView.addSubview(macImageView)
+    }
+
+    
+    func newImageViewWithImage(image: UIImage, frame: CGRect) -> UIImageView {
+        
+        let result = UIImageView(frame: frame)
+        result.contentMode = .scaleAspectFit
+        result.image = image
+        
+        return result
+    }
+    
+}
---
+++ UIScrollView/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  UIScrollView
+//
+//  Created by Marina Karpova on 04.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    var myScrollView = UIScrollView()
+    var myImageView = UIImageView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createScrollView()
+        
+    }
+    
+    
+    
+
+    //MARK: - UIScrollViewDelegate
+    
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let p = "Начинаеться прокрутка"
+//        print(p)
+//        print(scrollView.contentOffset.y)
+//        myScrollView.alpha = 0.50
+//    }
+//
+//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//        let p = "вызывается после прокрутки"
+//        print(p)
+//        myScrollView.alpha = 1.0
+//    }
+//
+//    func scrollViewDidEndDragging(_ scrolIView: UIScrollView, willDecelerate decelerate: Bool) {
+//        let p = "Гарантирует что вернет альфу к 1"
+//        print(p)
+//        self.myScrollView.alpha = 1.0
+//
+//    }
+    
+    
+    func createScrollView() {
+        let imageToLoad = UIImage(named: "mac")
+        myImageView = UIImageView(image: imageToLoad)
+        myScrollView = UIScrollView(frame: view.bounds)
+        myScrollView.addSubview(myImageView)
+        myScrollView.contentSize = myImageView.bounds.size
+        myScrollView.delegate = self
+        view.addSubview(myScrollView)
+    }
+}
+
